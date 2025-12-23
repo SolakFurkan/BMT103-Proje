b951590d4dfe8595e26d3b2bee715768a7f2fcfc
SolakFurkan <furkansolak.gazi@gmail.com>
2025-12-23 19:33:45 +0300

    Add files via upload

---
+++ Not_Yigilma_Bulma/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int not;
+    long long numara;
+}Ogrenci;
+
+int main() {
+    FILE *dosya;
+    Ogrenci ogr;
+    int frekans [101]={0};
+    int modNot=-1;
+    int maxSiklik=0;
+    //Her bilgisayarda çalıştırmak için ogrenci.dot dosyası C:\Users\<kullanıcıadı> içine atılmalı
+    //Kullanıcının masaüstü yolunu alma
+    char dosyaYolu[256];
+    char *userProfile=getenv("USERPROFILE"); //Kullanıcı adını almak için
+    if (userProfile == NULL) {
+        printf("Kullanici adi bulunamadi\n");
+        return 1;
+    }
+    //Kullanıcı ve masaüstü yolunu birleştirme
+    sprintf(dosyaYolu,"%s\\ogrenci.dat",userProfile);
+    printf("Dosya araniyor:%s\n",dosyaYolu);
+    dosya=fopen(dosyaYolu,"r");
+    if (dosya==NULL) {
+        printf("Kullanici adi bulunamadi\n");
+        return 1;
+    }
+
+    //Frekans hesaplama
+    while (fscanf(dosya,"%lld %d", &ogr.numara, &ogr.not)!=EOF) {
+        if (ogr.not>=0 && ogr.not<=100) {
+            frekans[ogr.not]++;
+        }
+    }
+
+    //Modu bulma
+    for (int i=0;i<101;i++) {
+        if (frekans[i]>maxSiklik) {
+            maxSiklik=frekans[i];
+            modNot=i;
+        }
+    }
+
+    //Ekrana yazdırma
+    if (modNot!=-1 && maxSiklik>0) {
+        printf("Sonuclar:\nYigilma olan not: %d (%d ogrenci\n", modNot, maxSiklik);
+        printf("---------------------\n");
+
+        rewind (dosya); //Dosyayı başa sarma
+        while (fscanf(dosya,"%lld %d", &ogr.numara, &ogr.not)!=EOF) {
+            if (ogr.not==modNot) {
+                printf("%lld\t%d\n",ogr.numara, ogr.not);
+            }
+        }
+
+    }
+    fclose(dosya);
+    return 0;
+}
